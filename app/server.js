6a977e797cd4fc877bec315183ecb001d53c6056
Nicoaedhel <monsieurhiboux@imac-de-monsieur.home>
2016-10-20 00:34:37 +0200

    Add sass-middleware

--- app/server.js
+++ app/server.js
@@ -1,5 +1,17 @@
 let express = require('express')
 let app = express()
+let sassMiddleware = require('node-sass-middleware');
+let path = require('path');
+
+app.use(sassMiddleware({
+    src: __dirname+'/scss',
+    dest: path.join(__dirname, 'public'),
+    debug: true,
+    outputStyle: 'compressed',
+    prefix : '/static'
+}))
+
+app.use("/static", express.static('public'))
 
 app.set('view engine', 'pug')
 
