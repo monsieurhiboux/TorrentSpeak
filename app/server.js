daf72148af9cd538afa4ba703fcbe8805be904a9
Nicoaedhel <monsieurhiboux@imac-de-monsieur.home>
2016-10-19 22:30:57 +0200

    Edit server.js

--- app/server.js
+++ app/server.js
@@ -1,10 +1,10 @@
 let express = require('express')
 let app = express()
 
-app.set('view engine', 'pug');
+app.set('view engine', 'pug')
 
 app.get('/', (request, response) =>{
-  response.render('layouts/app', { title: 'Hey', message: 'Hello there!'});
+  response.render('layouts/app', { title: 'Hey', message: 'Hello there!'})
 })
 
 app.listen(3000, () =>{
