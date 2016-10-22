e4797e8ab17eecb6c99160918d912d594e064198
Nicoaedhel <monsieurhiboux@iMac-de-Monsieur.local>
2016-10-22 15:42:00 +0200

    Edit BrowserWindow for electron

--- app/electron.js
+++ app/electron.js
@@ -1,7 +1,7 @@
 let electron = require('electron')
 
 electron.app.on("ready", function () {
-  var main = new electron.BrowserWindow({width: 530, height: 700});
+  var main = new electron.BrowserWindow({width: 530, height: 700, resizable: false, fullscreenable: false});
   main.on("closed", electron.app.quit);
   main.webContents.openDevTools();
   main.loadURL("http://127.0.0.1:3000/");
