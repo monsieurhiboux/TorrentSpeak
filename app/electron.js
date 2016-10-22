e37e73d56609c38db62176d3fcd305c60e33498d
Nicoaedhel <monsieurhiboux@iMac-de-Monsieur.local>
2016-10-22 19:29:34 +0200

    Edit app style

--- app/electron.js
+++ app/electron.js
@@ -1,11 +1,10 @@
 let electron = require('electron')
-
 require('electron-context-menu')({
     prepend: params => []
 });
-
 electron.app.on("ready", function () {
-  var main = new electron.BrowserWindow({width: 530, height: 700, resizable: false, fullscreenable: false});
+  var main = new electron.BrowserWindow({width: 530, height: 700, resizable: false, frame: false, fullscreenable: false});
+  main.setTitle(require('./package.json').name);
   main.on("closed", electron.app.quit);
   //main.webContents.openDevTools();
   main.loadURL("http://127.0.0.1:3000/");
@@ -15,9 +14,11 @@ electron.app.on("ready", function () {
         submenu: [
             { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
             { type: "separator" },
-            { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+            { label: "Quit", accelerator: "Command+Q", click: function() { electron.app.quit(); }}
         ]}
     ];
+
+
 
     electron.Menu.setApplicationMenu(electron.Menu.buildFromTemplate(template));
 });
